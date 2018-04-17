8e431dcf9dfcac4568f5cf90dd7f501bbfe6f9cf
Sebastian Latacz <sebastian@gitmate.io>
2018-04-17 16:48:01 +0200

    Setup tests

---
+++ tests/GitHub/test_github_milestone.py
@@ -0,0 +1,20 @@
+import os
+#import datetime
+
+from IGitt.GitHub import GitHubToken
+from IGitt.GitHub.GitHubMilestone import GitHubMilestone
+#from IGitt.GitHub.GitHubUser import GitHubUser
+#from IGitt.Interfaces import IssueStates
+
+from tests import IGittTestCase
+
+class GitHubMilestoneTest(IGittTestCase):
+
+    def setUp(self):
+        self.token = GitHubToken(os.environ.get('GITHUB_TEST_TOKEN', ''))
+        self.milestone = GitHubMilestone.create(self.token, 'gitmate-test-user', 'test', 'IGitt Created Test Milestone')
+        #self.milestone = GitHubMilestone(self.token, 'gitmate-test-user', 'test', 1)
+
+    def tearDown(self):
+        self.milestone.delete()
+
