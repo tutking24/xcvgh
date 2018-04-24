4e6e25fb355a86a74541f4f43407b87e8ef0689d
Sebastian Latacz <sebastian@gitmate.io>
2018-04-24 11:44:18 +0200

    Test Title and Description Setter

--- tests/GitLab/test_gitlab_projectmilestone.py
+++ tests/GitLab/test_gitlab_projectmilestone.py
@@ -20,6 +20,14 @@ class GitLabProjectMilestoneTest(IGittTestCase):
         self.preset_milestone = GitLabProjectMilestone(self.token, 'gitmate-test-user/test', 345808)
         self.assertEqual(self.preset_milestone.number, 345808)
 
+    def test_title_setter(self):
+        self.milestone.title = 'Test title in test_title_setter'
+        self.assertEqual(self.milestone.title, 'Test title in test_title_setter')
+
+    def test_description_setter(self):
+        self.milestone.description = 'Test description in test_description_setter'
+        self.assertEqual(self.milestone.description, 'Test description in test_description_setter')
+
 
     def tearDown(self):
         self.milestone.delete()
