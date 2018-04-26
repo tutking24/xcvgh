a991b85dede9838250f61b3bd8b1d131fbf4228d
Sebastian Latacz <sebastian@gitmate.io>
2018-04-26 11:10:04 +0200

    Squash with GitLab Tests Setup

--- tests/GitLab/test_gitlab_projectmilestone.py
+++ tests/GitLab/test_gitlab_projectmilestone.py
@@ -13,8 +13,7 @@ class GitLabProjectMilestoneTest(IGittTestCase):
 
     def setUp(self):
         self.token = GitLabOAuthToken(os.environ.get('GITLAB_TEST_TOKEN', ''))
-        self.milestone = GitLabProjectMilestone.create(self.token, 'gitmate-test-user/test', 'For IGitt tests created test milestone NEW TITEL')
-        self.day_created = datetime.datetime.utcnow().day # Gets a datetime object of the day of the creation
+        self.milestone = GitLabProjectMilestone(self.token, 'gitmate-test-user/test', 513937)
 
     def test_setUp(self):
         assert(isinstance(self.milestone, GitLabProjectMilestone))
