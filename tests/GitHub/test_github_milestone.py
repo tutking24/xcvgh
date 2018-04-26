64ed3a63cff21bd81d0bf76ad0b16dc0b575bd46
Sebastian Latacz <sebastian@gitmate.io>
2018-04-26 16:01:30 +0200

    mend

--- tests/GitHub/test_github_milestone.py
+++ tests/GitHub/test_github_milestone.py
@@ -10,20 +10,22 @@ from IGitt.GitHub.GitHubRepository import GitHubRepository
 
 from tests import IGittTestCase
 
-class GitHubMilestoneTest(IGittTestCase):
 
+class GitHubMilestoneTest(IGittTestCase):
     def setUp(self):
         self.token = GitHubToken(os.environ.get('GITHUB_TEST_TOKEN', ''))
-        self.milestone = GitHubMilestone(self.token, 'gitmate-test-user', 'test', 1)
+        self.milestone = GitHubMilestone(self.token, 'gitmate-test-user',
+                                         'test', 1)
 
     def test_setUp(self):
-        assert(isinstance(self.milestone, GitHubMilestone))
+        assert (isinstance(self.milestone, GitHubMilestone))
 
     def test_number(self):
         self.assertEqual(self.milestone.number, 1)
 
     def test_title_getter(self):
-        self.assertEqual(self.milestone.title, 'Permanent IGitt test milestone. DO NOT DELETE.')
+        self.assertEqual(self.milestone.title,
+                         'Permanent IGitt test milestone. DO NOT DELETE.')
 
     def test_title_setter(self):
         self.milestone.title = 'Updated Title'
@@ -32,7 +34,8 @@ class GitHubMilestoneTest(IGittTestCase):
 
     def test_description_setter(self):
         self.milestone.description = 'Test Milestone Description'
-        self.assertEqual(self.milestone.description, 'Test Milestone Description')
+        self.assertEqual(self.milestone.description,
+                         'Test Milestone Description')
         self.milestone.description = None
 
     def test_state_getter(self):
@@ -46,41 +49,53 @@ class GitHubMilestoneTest(IGittTestCase):
         self.assertEqual(self.milestone.state, MilestoneStates.OPEN)
 
     def test_created_getter(self):
-        self.assertEqual(self.milestone.created, datetime.datetime(2018, 4, 24, 12, 19, 49))
-
+        self.assertEqual(self.milestone.created,
+                         datetime.datetime(2018, 4, 24, 12, 19, 49))
 
     def test_updated_getter(self):
         self.milestone.description = 'Description update to test updated getter'
-        self.assertEqual(self.milestone.updated.date(), datetime.datetime(2018, 4, 25).date())
+        self.assertEqual(self.milestone.updated.date(),
+                         datetime.datetime(2018, 4, 25).date())
         self.milestone.description = None
 
     def test_due_date_setter(self):
         self.milestone.due_date = datetime.datetime(2050, 4, 23, 7)
-        self.assertEqual(self.milestone.due_date, datetime.datetime(2050, 4, 23, 7))
+        self.assertEqual(self.milestone.due_date,
+                         datetime.datetime(2050, 4, 23, 7))
         self.milestone.due_date = None
         self.assertEqual(self.milestone.due_date, None)
 
     def test_issues_getter(self):
-        self.assertEqual({int(issue.number) for issue in self.milestone.issues},
-                         {136, 138, 139}) # Prüft nicht, ob ein set zurück kommt?
+        self.assertEqual(
+            {int(issue.number)
+             for issue in self.milestone.issues},
+            {136, 138, 139})  # Prüft nicht, ob ein set zurück kommt?
 
     def test_merge_requests_getter(self):
-        self.assertEqual({int(merge_request.number) for merge_request in self.milestone.merge_requests},
-                         {140})
+        self.assertEqual({
+            int(merge_request.number)
+            for merge_request in self.milestone.merge_requests
+        }, {140})
 
     def test_start_date_getter(self):
         self.assertEqual(self.milestone.start_date, None)
 
     def test_project_getter(self):
-        assert(isinstance(self.milestone.project, GitHubRepository))
+        assert (isinstance(self.milestone.project, GitHubRepository))
 
     def test_create_delete_methods(self):
-        self.minimal_data_milestone = GitHubMilestone.create(self.token, 'gitmate-test-user', 'test', 'Temporary created test milestone.')
-        assert(isinstance(self.minimal_data_milestone, GitHubMilestone))
-        self.assertEqual(self.minimal_data_milestone.title, 'Temporary created test milestone.')
+        self.minimal_data_milestone = GitHubMilestone.create(
+            self.token, 'gitmate-test-user', 'test',
+            'Temporary created test milestone.')
+        assert (isinstance(self.minimal_data_milestone, GitHubMilestone))
+        self.assertEqual(self.minimal_data_milestone.title,
+                         'Temporary created test milestone.')
         self.minimal_data_milestone.delete()
 
-        self.full_data_milestone = GitHubMilestone.create(self.token, 'gitmate-test-user', 'test', 'Temporary created test milestone.', MilestoneStates.OPEN, 'Test Description', datetime.datetime(2050, 4, 23, 7))
-        assert(isinstance(self.full_data_milestone, GitHubMilestone))
+        self.full_data_milestone = GitHubMilestone.create(
+            self.token, 'gitmate-test-user', 'test',
+            'Temporary created test milestone.', MilestoneStates.OPEN,
+            'Test Description', datetime.datetime(2050, 4, 23, 7))
+        assert (isinstance(self.full_data_milestone, GitHubMilestone))
         self.assertEqual(self.full_data_milestone.state, MilestoneStates.OPEN)
         self.full_data_milestone.delete()
--- tests/GitLab/test_gitlab_projectmilestone.py
+++ tests/GitLab/test_gitlab_projectmilestone.py
@@ -9,20 +9,22 @@ from IGitt.GitLab.GitLabRepository import GitLabRepository
 
 from tests import IGittTestCase
 
-class GitLabProjectMilestoneTest(IGittTestCase):
 
+class GitLabProjectMilestoneTest(IGittTestCase):
     def setUp(self):
         self.token = GitLabOAuthToken(os.environ.get('GITLAB_TEST_TOKEN', ''))
-        self.milestone = GitLabProjectMilestone(self.token, 'gitmate-test-user/test', 513937)
+        self.milestone = GitLabProjectMilestone(
+            self.token, 'gitmate-test-user/test', 513937)
 
     def test_setUp(self):
-        assert(isinstance(self.milestone, GitLabProjectMilestone))
+        assert (isinstance(self.milestone, GitLabProjectMilestone))
 
     def test_number_getter(self):
         self.assertEqual(self.milestone.number, 513937)
 
     def test_title_getter(self):
-        self.assertEqual(self.milestone.title, 'Permanent IGitt test milestone. DO NOT DELETE.')
+        self.assertEqual(self.milestone.title,
+                         'Permanent IGitt test milestone. DO NOT DELETE.')
 
     def test_title_setter(self):
         self.milestone.title = 'Updated Title'
@@ -31,7 +33,8 @@ class GitLabProjectMilestoneTest(IGittTestCase):
 
     def test_description_setter(self):
         self.milestone.description = 'Test Milestone Description'
-        self.assertEqual(self.milestone.description, 'Test Milestone Description')
+        self.assertEqual(self.milestone.description,
+                         'Test Milestone Description')
         self.milestone.description = None
 
     def test_state_getter(self):
@@ -45,43 +48,59 @@ class GitLabProjectMilestoneTest(IGittTestCase):
         self.assertEqual(self.milestone.state, MilestoneStates.OPEN)
 
     def test_created_getter(self):
-        self.assertEqual(self.milestone.created, datetime.datetime(2018, 4, 25, 9, 11, 0, 29000))
+        self.assertEqual(self.milestone.created,
+                         datetime.datetime(2018, 4, 25, 9, 11, 0, 29000))
 
     def test_updated_getter(self):
         self.milestone.description = 'Description update to test updated getter'
-        self.assertEqual(self.milestone.updated.date(), datetime.datetime(2018, 4, 26).date())
+        self.assertEqual(self.milestone.updated.date(),
+                         datetime.datetime(2018, 4, 26).date())
         self.milestone.description = None
 
     def test_due_date_setter(self):
         self.milestone.due_date = datetime.datetime(2050, 4, 23, 0, 0)
-        self.assertEqual(self.milestone.due_date, datetime.datetime(2050, 4, 23, 0, 0))
+        self.assertEqual(self.milestone.due_date,
+                         datetime.datetime(2050, 4, 23, 0, 0))
         self.milestone.due_date = None
         self.assertEqual(self.milestone.due_date, None)
 
     def test_start_date_setter(self):
         self.milestone.start_date = datetime.datetime(2040, 4, 23, 0, 0)
-        self.assertEqual(self.milestone.start_date, datetime.datetime(2040, 4, 23, 0, 0))
+        self.assertEqual(self.milestone.start_date,
+                         datetime.datetime(2040, 4, 23, 0, 0))
         self.milestone.start_date = None
         self.assertEqual(self.milestone.start_date, None)
 
     def test_issues_getter(self):
-        self.assertEqual({int(issue.number) for issue in self.milestone.issues},
-                         {37, 38, 39}) # Prüft nicht, ob ein set zurück kommt?
+        self.assertEqual(
+            {int(issue.number)
+             for issue in self.milestone.issues},
+            {37, 38, 39})  # Prüft nicht, ob ein set zurück kommt?
 
     def test_merge_requests_getter(self):
-        self.assertEqual({int(merge_request.number) for merge_request in self.milestone.merge_requests},
-                         {76})
+        self.assertEqual({
+            int(merge_request.number)
+            for merge_request in self.milestone.merge_requests
+        }, {76})
 
     def test_project_getter(self):
-        assert(isinstance(self.milestone.project, GitLabRepository))
+        assert (isinstance(self.milestone.project, GitLabRepository))
 
     def test_create_delete_methods(self):
-        self.minimal_data_milestone = GitLabProjectMilestone.create(self.token, 'gitmate-test-user/test', 'Temporary created minimal data test milestone.')
-        assert(isinstance(self.minimal_data_milestone, GitLabProjectMilestone))
-        self.assertEqual(self.minimal_data_milestone.title, 'Temporary created minimal data test milestone.')
+        self.minimal_data_milestone = GitLabProjectMilestone.create(
+            self.token, 'gitmate-test-user/test',
+            'Temporary created minimal data test milestone.')
+        assert (isinstance(self.minimal_data_milestone,
+                           GitLabProjectMilestone))
+        self.assertEqual(self.minimal_data_milestone.title,
+                         'Temporary created minimal data test milestone.')
         self.minimal_data_milestone.delete()
 
-        self.full_data_milestone = GitLabProjectMilestone.create(self.token, 'gitmate-test-user/test', 'Temporary created full data test milestone.', 'Test Description', datetime.datetime(2050, 4, 23), datetime.datetime(2040, 4, 23))
-        assert(isinstance(self.full_data_milestone, GitLabProjectMilestone))
-        self.assertEqual(self.full_data_milestone.start_date, datetime.datetime(2040, 4, 23))
+        self.full_data_milestone = GitLabProjectMilestone.create(
+            self.token, 'gitmate-test-user/test',
+            'Temporary created full data test milestone.', 'Test Description',
+            datetime.datetime(2050, 4, 23), datetime.datetime(2040, 4, 23))
+        assert (isinstance(self.full_data_milestone, GitLabProjectMilestone))
+        self.assertEqual(self.full_data_milestone.start_date,
+                         datetime.datetime(2040, 4, 23))
         self.full_data_milestone.delete()
