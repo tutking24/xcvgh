8283e5318e8411a9c010530e5ed89eb3a7ce49f4
Sebastian Latacz <sebastian@gitmate.io>
2018-07-25 11:47:17 +0200

    Extract Methods from Class

--- IGitt/Interfaces/TimeRecord.py
+++ IGitt/Interfaces/TimeRecord.py
@@ -13,17 +13,14 @@ from IGitt.Interfaces.Comment import Comment
 from IGitt.Interfaces.Comment import CommentType
 from IGitt.Interfaces.Repository import Repository
 
-@staticmethod
 def comment_contains_time_record(comment_to_read_from: Comment):
-    time_record_regex = re.compile()
 
     if (re.match(r'(subtracted|added)(\s\d[wdhms]){1,5}(\sof time spent$)',
         comment_to_read_from.data['body']) or
         comment_to_read_from.data['body'] == 'removed time spent'):
         return True
 
-@staticmethod
-def extract_loged_time_from_comment(comment_to_read_from) -> timedelta:
+def extract_loged_time_from_comment(comment_to_read_from: Comment) -> timedelta:
     pass
 
 class TimeRecord(IGittObject):
