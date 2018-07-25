c07656159469debd77ac639beb71cc5d167f5d64
Sebastian Latacz <sebastian@gitmate.io>
2018-07-25 11:46:06 +0200

    Add tests for record checker

---
+++ tests/Interfaces/test_time_record.py
@@ -0,0 +1,36 @@
+import os
+
+from IGitt.Interfaces import TimeRecord
+from IGitt.GitLab import GitLabOAuthToken
+from IGitt.GitLab.GitLabComment import GitLabComment
+from IGitt.Interfaces.Comment import CommentType
+
+from tests import IGittTestCase
+
+
+class TestTimeRecord(IGittTestCase):
+
+    def setUp(self):
+        token = GitLabOAuthToken(os.environ.get('GITLAB_TEST_TOKEN', ''))
+        self.time_subtracting_comment = GitLabComment(token,
+                                     'gitmate-test-user/test', 42,
+                                     CommentType.ISSUE, 88185697)
+        self.time_adding_comment = GitLabComment(token,
+                                     'gitmate-test-user/test', 42,
+                                     CommentType.ISSUE, 88185691)
+        self.time_reseting_comment = GitLabComment(token,
+                                     'gitmate-test-user/test', 42,
+                                     CommentType.ISSUE, 88178597)
+        self.comment_without_time_record = GitLabComment(token,
+                                           'gitmate-test-user/test', 42,
+                                           CommentType.ISSUE, 88744093)
+
+    def test_comment_contains_time_record(self):
+        self.assertTrue(TimeRecord.comment_contains_time_record(
+                                        self.time_subtracting_comment))
+        self.assertTrue(TimeRecord.comment_contains_time_record(
+                                        self.time_adding_comment))
+        self.assertTrue(TimeRecord.comment_contains_time_record(
+                                        self.time_reseting_comment))
+        self.assertFalse(TimeRecord.comment_contains_time_record(
+                                        self.comment_without_time_record))
