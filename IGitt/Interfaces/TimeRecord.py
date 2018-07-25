3fe293951203e333a95dac2989282babca0337f1
Sebastian Latacz <sebastian@gitmate.io>
2018-07-25 18:42:05 +0200

    Implement extract_loged_time_from_comment

--- IGitt/Interfaces/TimeRecord.py
+++ IGitt/Interfaces/TimeRecord.py
@@ -21,7 +21,28 @@ def comment_contains_time_record(comment_to_read_from: Comment):
         return True
 
 def extract_loged_time_from_comment(comment_to_read_from: Comment) -> timedelta:
-    pass
+
+    if re.match(r'(subtracted|added)(\s\d+[wdhms]){1,5}(\sof time spent$)',
+        comment_to_read_from.data['body']):
+
+        time_values = {'s':0, 'm':0, 'h':0, 'd':0, 'w':0}
+
+
+        for time_unit in ['s', 'm', 'h', 'd', 'w']:
+            time_extraction_regex = r'(?:\s)(\d+)(?:' + re.escape(time_unit) + r')'
+
+            if not re.search(time_extraction_regex, comment_to_read_from.data['body']):
+                pass
+
+            else:
+                time_values[time_unit] = int(re.search(time_extraction_regex, comment_to_read_from.data['body']).group(1))
+
+        extracted_time = timedelta(seconds=time_values['s'], minutes=time_values['m'], hours=time_values['h'], days=time_values['d'], weeks=time_values['w'])
+
+    if re.search(r'(^subtracted)', comment_to_read_from.data['body']):
+        extracted_time = extracted_time * (-1)
+
+    return extracted_time
 
 class TimeRecord(IGittObject):
 
